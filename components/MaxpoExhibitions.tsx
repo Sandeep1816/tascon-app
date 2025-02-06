6353523ae93aef52c5cd4323c6a63cfa40a3825a
unknown <chvamshi03@gmail.com>
2025-02-06 17:05:14 +0530

    added maxpo detailes

--- components/MaxpoExhibitions.tsx
+++ components/MaxpoExhibitions.tsx
@@ -23,7 +23,7 @@ const MaxpoExhibitions = () => {
           rel="noopener noreferrer"
           className="mt-6 inline-block bg-blue-600 text-white px-6 py-3 rounded-lg shadow hover:bg-blue-700 transition"
         >
-          Visit 
+          Visit Site
         </a>
       </div>
     </div>
