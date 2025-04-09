0f0b2f470115933ecc2cf711cf4f2ba8f25b7615
Sandeep1816 <sandeepavala2@gmail.com>
2025-04-09 13:02:50 +0530

    email subject changed

--- app/api/sendEmail/route.tsx
+++ app/api/sendEmail/route.tsx
@@ -17,7 +17,7 @@ export async function POST(req: NextRequest) {
 
     // Email content
     const mailOptions = {
-        from: "chvamshi03@gmail.com",
+        from: "Tascon<chvamshi03@gmail.com>",
         to: "info@tasconmedia.com, tarannum.s@tasconmedia.com",
         subject: "New Enquiry from TASCON Contact Form",
         html: `
